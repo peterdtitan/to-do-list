3b664ffc1a6a8f3fdea4a24fae27a1dcb8e8de8d
Peter Okorafor <peterdtitan@gmail.com>
2022-07-13 12:12:51 +0300

    Project structure and functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import _ from 'lodash';
+import './style.scss';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const tasks = [
+  {
+    description: 'Do my laundry',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Restart Capstone project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Start JavaScript Refresher',
+    completed: false,
+    index: 2,
+  },
+];
+
+const tasksList = document.getElementById('tasks-list');
+tasks.forEach((task) => {
+  tasksList.innerHTML += `
+    <li><hr></li>
+    <li class="task">
+        <div>
+            <input type="checkbox" name="checkbox-${task.index}" ${task.completed ? 'checked' : 'unchecked'}>
+            <h2>${task.description}</h2>
+        </div>
+        <button><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
+    </li>
+  `;
+}); 
